a9361d9b513748505b9cb63658bb53601a289c2f
Diego Riano <diriano@gmail.com>
2024-04-18 17:53:05 -0300

    adding gene IDs

--- preprocessing.R
+++ preprocessing.R
@@ -12,6 +12,12 @@ targets$Individual<-as.factor(targets$Individual)
 targets$DevStage<-as.factor(targets$DevStage)
 targets$LibraryName<-as.factor(targets$LibraryName)
 targets
+
+geneIDs_GALBA_dups<-read.delim("geneIDs_GALBA_dups.txt",header=T)
+saveRDS(geneIDs_GALBA_dups$Name, 'geneIDs_GALBA_dups.rds')
+
+geneIDs_BRAKER3_dups<-read.delim("geneIDs_BRAKER3_dups.txt",header=T)
+saveRDS(geneIDs_BRAKER3_dups$Name, 'geneIDs_BRAKER3_dups.rds')
 #targets<-targets[which(! targets$SampleName %in% c('SRR1979663')),]
 
 #Getting tx2gene files, we have four collections of genes two for BRAKER and two for GALBA. There are two of each of these, because one includes duplicates and the other do not.
@@ -28,7 +34,7 @@ all(file.exists(myFiles_BRAKER3))
 txi.salmon_BRAKER3<-tximport(files = myFiles_BRAKER3, type = 'salmon', tx2gene = tx2gene_BRAKER3, txIn = TRUE, txOut = FALSE)
 head(txi.salmon_BRAKER3$counts)
 dataTPM_BRAKER3<-txi.salmon_BRAKER3$abundance
-dataTPM_BRAKER3_melt<-melt(dataTPM_BRAKER3)
+dataTPM_BRAKER3_melt<-melt(dataTPM_BRAKER3[which(rowSums(dataTPM_BRAKER3)>0),])
 colnames(dataTPM_BRAKER3_melt)<-c('Gene','Sample','TPM')
 dataTPM_BRAKER3_melt$DevStage<-NA
 dataTPM_BRAKER3_melt[which(dataTPM_BRAKER3_melt$Sample %in% c('SC_163P','SC_138P','SC_235P','SC_234P')),'DevStage']<-'P'
@@ -50,8 +56,8 @@ all(file.exists(myFiles_BRAKER3_dups))
 txi.salmon_BRAKER3_dups<-tximport(files = myFiles_BRAKER3_dups, type = 'salmon', tx2gene = tx2gene_BRAKER3_dups, txIn = TRUE, txOut = FALSE)
 head(txi.salmon_BRAKER3_dups$counts)
 dataTPM_BRAKER3_dups<-txi.salmon_BRAKER3_dups$abundance
-dataTPM_BRAKER3_dups_melt<-melt(dataTPM_BRAKER3_dups)
-colnames(dataTPM_BRAKER3_dups_melt)<-c('Gene','DevStage','TPM')
+dataTPM_BRAKER3_dups_melt<-melt(dataTPM_BRAKER3_dups[which(rowSums(dataTPM_BRAKER3_dups)>0),])
+colnames(dataTPM_BRAKER3_dups_melt)<-c('Gene','Sample','TPM')
 dataTPM_BRAKER3_dups_melt$DevStage<-NA
 dataTPM_BRAKER3_dups_melt[which(dataTPM_BRAKER3_dups_melt$Sample %in% c('SC_163P','SC_138P','SC_235P','SC_234P')),'DevStage']<-'P'
 dataTPM_BRAKER3_dups_melt[which(dataTPM_BRAKER3_dups_melt$Sample %in% c('SC_234M','SC_163M','SC_138M','SC_235M')),'DevStage']<-'M'
@@ -68,8 +74,8 @@ all(file.exists(myFiles_GALBA))
 txi.salmon_GALBA<-tximport(files = myFiles_GALBA, type = 'salmon', tx2gene = tx2gene_GALBA, txIn = TRUE, txOut = FALSE)
 head(txi.salmon_GALBA$counts)
 dataTPM_GALBA<-txi.salmon_GALBA$abundance
-dataTPM_GALBA_melt<-melt(dataTPM_GALBA)
-colnames(dataTPM_GALBA_melt)<-c('Gene','DevStage','TPM')
+dataTPM_GALBA_melt<-melt(dataTPM_GALBA[which(rowSums(dataTPM_GALBA)>0),])
+colnames(dataTPM_GALBA_melt)<-c('Gene','Sample','TPM')
 dataTPM_GALBA_melt$DevStage<-NA
 dataTPM_GALBA_melt[which(dataTPM_GALBA_melt$Sample %in% c('SC_163P','SC_138P','SC_235P','SC_234P')),'DevStage']<-'P'
 dataTPM_GALBA_melt[which(dataTPM_GALBA_melt$Sample %in% c('SC_234M','SC_163M','SC_138M','SC_235M')),'DevStage']<-'M'
@@ -83,8 +89,8 @@ all(file.exists(myFiles_GALBA_dups))
 txi.salmon_GALBA_dups<-tximport(files = myFiles_GALBA_dups, type = 'salmon', tx2gene = tx2gene_GALBA_dups, txIn = TRUE, txOut = FALSE)
 head(txi.salmon_GALBA_dups$counts)
 dataTPM_GALBA_dups<-txi.salmon_GALBA_dups$abundance
-dataTPM_GALBA_dups_melt<-melt(dataTPM_GALBA_dups)
-colnames(dataTPM_GALBA_dups_melt)<-c('Gene','DevStage','TPM')
+dataTPM_GALBA_dups_melt<-melt(dataTPM_GALBA_dups[which(rowSums(dataTPM_GALBA_dups)>0),])
+colnames(dataTPM_GALBA_dups_melt)<-c('Gene','Sample','TPM')
 dataTPM_GALBA_dups_melt$DevStage<-NA
 dataTPM_GALBA_dups_melt[which(dataTPM_GALBA_dups_melt$Sample %in% c('SC_163P','SC_138P','SC_235P','SC_234P')),'DevStage']<-'P'
 dataTPM_GALBA_dups_melt[which(dataTPM_GALBA_dups_melt$Sample %in% c('SC_234M','SC_163M','SC_138M','SC_235M')),'DevStage']<-'M'
