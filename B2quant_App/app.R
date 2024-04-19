b676618ae24218b928607f38dcd95222968046e7
Diego Mauricio Riaño-Pachón <diriano@gmail.com>
2024-04-19 10:41:00 -0300

    working version of the app

--- B2quant_App/app.R
+++ B2quant_App/app.R
@@ -1,24 +1,28 @@
 library(shiny)
-library(reshape2)
-source("helper.R")
 library(ggplot2)
 rm(list=ls())
 
 ui <- fluidPage(
   # Title of the APP: Sugarcane EST-DigitalNorthern
-  titlePanel(title=div(img(src="LabBCES.png"), "RNA-Seq data Leaf Development again Genome SP80-3280 B2 - LabBCES")),
+  titlePanel(title=div(img(src="LabBCES.png"), "RNA-Seq data Leaf Development against Genome SP80-3280 B2 - LabBCES")),
   fluidRow(
     p("Expression profiles from sugarcane using genome annotation of assembly B2 and RNA-Seq data from leaf development (", a(href='https://pubmed.ncbi.nlm.nih.gov/26714767/', 
-    'Mattiello et al., 2015'),").",
+    'Mattiello et al., 2015'),"). In that publication we measure gene expression using RNA-Seq in four leaf segments of sugarcane SP80-3280. B0: Base Zero, B: Base, M: Middle part, P: Tip of leaf",
+      style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
+    
+    p("The B2 genome assembly of cultivar SP80-3280 is a draft assembly generated with PacBio HiFi and Illumina HiC reads. It has a high N50.",
       style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
     
     br(),
-    p("We have two alternative annotations for the time being, one generated with GALBA and another with BRAKER3. The anotation with GALBA uses as extrisic evidence, for gene prediction, proteins sequences. While, BRAKER# uses both RNA-Seq data and protein sequences."
+    p("We have two alternative annotations for the time being using the B2 assembly. One generated with GALBA and another with BRAKER3. The anotation with GALBA uses as extrinsic evidence, for gene prediction, proteins sequences. BRAKER3 uses both RNA-Seq data and protein sequences."
     ,style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
     
     br(),
-    p("The goal of this tool is to aid you in visualizing the expression profile of a selected gene of interest.
-    Users can control this with the slider ",strong('Percent of libraries with data:'),"."
+    p("The goal of this tool is to aid you in visualizing the expression profile of a selected (knonw) gene of interest. 
+    You could select a gene of interest based on similarity searches using our ", a(href='http://labbces.cena.usp.br:4567/','Blast server'),".
+      In the BLAST server the identifiers of the annotated transcriptional units and their deduced proteins have the following form XXXXX_gNNNNNN.tY, where XXXXX could either be BRAKER3 or GALBA,
+      NNNNNN and Y are numbers. But please note that the expression values available here are for the gene, and not the transcriptional units. So in order to convert the identifiers from the transcript to the gene, you just need to remove the suffix .tN.
+      for example for the transcript: BRAKER3_g152127.t2, the correspoding gene name that you can use here would be BRAKER3_g152127. Please also check that you select the proper dataset for the given gene identifier." 
       ,style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
     
     br(),
@@ -30,7 +34,7 @@ ui <- fluidPage(
         inputId="selectedDataset",
         label = "Select dataset, either GALBA or BRAKER3",
         choices = c('GALBA','BRAKER3'),
-        selected = NULL,
+        selected = 'BRAKER3',
         multiple = FALSE,
         selectize = TRUE,
         width = NULL,
@@ -46,29 +50,29 @@ ui <- fluidPage(
   p("The last figure can display the expression profile of each of the genes/transcripts kepth after the filters."
     ,style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
   
+  # br(),
+  # sidebarLayout(
+  #   sidebarPanel(
+  #     #htmlOutput("listSelectedGenesUI"),
+  #     htmlOutput("listSelectedGenesUI2"),
+  #   ),
+  #   mainPanel(
+  #     plotOutput(outputId = "expressionProfileSelectedGene"),
+  #     tableOutput(outputId = "expressionTableSelectedGene")
+  #   )
+  # ),
   br(),
   sidebarLayout(
     sidebarPanel(
-      #htmlOutput("listSelectedGenesUI"),
-      htmlOutput("listSelectedGenesUI2"),
+      textInput(inputId='selectedGene',
+                label='Select Gene of Interest',
+                value = NULL,
+                width = NULL,
+                placeholder = "Type Gene ID")
     ),
     mainPanel(
       plotOutput(outputId = "expressionProfileSelectedGene"),
       tableOutput(outputId = "expressionTableSelectedGene")
-    )
-  ),
-  p("Finaly, the user can download the whole dataset (all genes/transcripts), or the set after filtering.z"
-    ,style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
-  
-  br(),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("dataset", "Choose a dataset:",
-                  choices = c("full", "filtered")),
-      downloadButton("downloadData", "Download")
-    ),
-    mainPanel(
-      tableOutput("table")
     )
   ),
   hr(),
@@ -83,29 +87,18 @@ ui <- fluidPage(
 server <- function(input, output) {
   dataset <- reactive({
     fileNameTPM=paste("dataTPM_",input$selectedDataset,"_dups.rds",sep="")
-    fileNameGeneIDs=paste("geneIDs_",input$selectedDataset,"_dups.rds",sep="")
+    # fileNameGeneIDs=paste("geneIDs_",input$selectedDataset,"_dups.rds",sep="")
     urlSourceTPM=paste("https://raw.githubusercontent.com/labbces/B2quant/main/",fileNameTPM,sep="")
-    urlSourceGeneIDs=paste("https://raw.githubusercontent.com/labbces/B2quant/main/",fileNameGeneIDs,sep="")
+    # urlSourceGeneIDs=paste("https://raw.githubusercontent.com/labbces/B2quant/main/",fileNameGeneIDs,sep="")
     download.file(urlSourceTPM, fileNameTPM)
-    download.file(urlSourceGeneIDs, fileNameGeneIDs)
+    # download.file(urlSourceGeneIDs, fileNameGeneIDs)
     dataTPM<-readRDS(fileNameTPM)
-    geneIDs<-readRDS(fileNameGeneIDs)
-    return(list(dataTPM=dataTPM,geneIDs=geneIDs))
+    # geneIDs<-readRDS(fileNameGeneIDs)
+    # return(list(dataTPM=dataTPM,geneIDs=geneIDs))
+    return(list(dataTPM=dataTPM))
   })
-  
-  #Boxplot of genes expression values per EST library BRAKER3_dups.rds
-  # dataset1<-reactive({
-  #   input$selectedDataset
-  # })
-  # fileName=paste("dataTPM_",dataset1(),"_dups.rds")
-  # urlSource=paste("https://raw.githubusercontent.com/labbces/B2quant/main/",fileName)
-  # download.file(urlSource)
-  # 
+
   output$boxPlotExpressedGenes <- renderPlot({
-    # fileName=paste("dataTPM_",input$selectedDataset,"_dups.rds",sep="")
-    # urlSource=paste("https://raw.githubusercontent.com/labbces/B2quant/main/",fileName,sep="")
-    # download.file(urlSource, fileName)
-    # dataTPM<-readRDS(fileName)
     dataTPM<-dataset()$dataTPM
     ggplot(dataTPM, aes(x=Sample,y=TPM,fill=DevStage)) +
       theme_bw()+
@@ -118,16 +111,6 @@ server <- function(input, output) {
   output$selectedAnnotationReference <- renderText({
     paste("You have selected: ",input$selectedDataset)
   })
-  output$listSelectedGenesUI2 <-renderUI({
-    #fileName=paste("dataTPM_",input$selectedDataset,"_dups.rds",sep="")
-    #dataTPM<-readRDS(fileName)
-    geneIDs<-dataset()$geneIDs
-    selectInput(
-    "selectedGene",p("Select the gene of interest, based on your previous filters:",
-                     style="color:black; text-align:center"),
-    choices=geneIDs[1:100]
-  )
-  })
   output$expressionProfileSelectedGene<-renderPlot({
     dataTPM<-dataset()$dataTPM
     ggplot(dataTPM[which(dataTPM$Gene == input$selectedGene),],aes(x=Sample, y=TPM, fill=DevStage))+
@@ -135,30 +118,12 @@ server <- function(input, output) {
       theme(legend.position = 'none',axis.text.x = element_text(angle = 45)) +
       geom_col() +
       ylab("TPM - Log10")+
-      scale_y_log10() +
       ggtitle(input$selectedGene)
   })
   output$expressionTableSelectedGene<-renderTable({
     dataTPM<-dataset()$dataTPM
     dataTPM[which(dataTPM$Gene == input$selectedGene),]
   })
-  datasetDownload <- reactive({
-    switch(input$dataset,
-           "full" = dataCPM,
-           "filtered" = getExpressionProfile(dataCPM, input$minCPM,input$selectLib, input$percentLibs, libraries)
-           )
-  })
-  output$table <- renderText({
-    paste("Ready to download your selected dataset: ", input$dataset, "with ", nrow(datasetDownload()), "genes" )
-  })
-  output$downloadData <- downloadHandler(
-    filename = function() {
-      paste(input$dataset, "_sugarcaneEST_CPM.csv", sep = "")
-    },
-    content = function(file) {
-      write.csv(datasetDownload(), file, row.names = TRUE)
-      }
-  )
 }
 
 shinyApp(ui = ui, server = server)
